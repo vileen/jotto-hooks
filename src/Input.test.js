551bd19d07ca4f1e896d5eae7bfbad9fa54729df
Dominik Soczewka <dominik.soczewka@radarservices.com>
2019-10-31 16:10:07 +0100

    Add basic components

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
-import App from './App';
+import Input from './Input';
 
 /**
  * Setup function for app component.
  * @returns {ShallowWrapper}
  */
 const setup = () => {
-  return shallow(<App />);
+  return shallow(<Input />);
 };
 
-it('App renders without error', () => {
+it('Input renders without error', () => {
   const wrapper = setup();
-  const component = findByTestAttr(wrapper, 'component-app');
+  const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
 });
