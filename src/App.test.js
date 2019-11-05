7c7d368554962bc1712f6e3e032d11dadb7e5557
Dominik Soczewka <dominik.soczewka@radarservices.com>
2019-11-05 14:54:13 +0100

    Add loader and finish tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,11 +9,20 @@ const mockGetSecretWord = jest.fn();
 
 /**
  * Setup function for app component.
+ * @param {string} secretWord - desired secretWord state value for test
  * @returns {ShallowWrapper}
  */
-const setup = () => {
+const setup = (secretWord = 'party') => {
   mockGetSecretWord.mockClear();
   hookActions.getSecretWord = mockGetSecretWord;
+
+  const mockUseReducer = jest.fn()
+    .mockReturnValue([
+      { secretWord },
+      function() {}
+    ]);
+
+  React.useReducer = mockUseReducer;
 
   // use mount because useEffect is not called on shallow
   // https://github.com/airbnb/enzyme/issues/2086
@@ -43,5 +52,51 @@ describe('getSecretWord calls', () => {
     wrapper.setProps();
 
     expect(mockGetSecretWord).toHaveBeenCalledTimes(1); // only on setup
+  });
+});
+
+describe('secretWord is not null', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup('party');
+  });
+
+  it('renders app when secretWord is not null', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+
+    // exists:
+    // - no params - check if any node is returned
+    // - selector - checks if any element matches the selector
+    expect(appComponent.exists()).toBe(true);
+  });
+
+  it('does not render spinner when secretWord is not null', () => {
+    const spinnerComponent = findByTestAttr(wrapper, 'spinner');
+
+    expect(spinnerComponent.exists()).toBe(false);
+  });
+});
+
+describe('secretWord is null', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup(null);
+  });
+
+  it('does not render app when secretWord is not null', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+
+    // exists:
+    // - no params - check if any node is returned
+    // - selector - checks if any element matches the selector
+    expect(appComponent.exists()).toBe(false);
+  });
+
+  it('renders spinner when secretWord is not null', () => {
+    const spinnerComponent = findByTestAttr(wrapper, 'spinner');
+
+    expect(spinnerComponent.exists()).toBe(true);
   });
 });
